a2421fb5a3c1bb67f0222d7df8d19940a16fd889
Masoud <gharagozlu.masoud@gmail.com>
2018-03-11 20:01:13 +0330

    add google code

--- themes/frontend/views/partial-views/_footer.php
+++ themes/frontend/views/partial-views/_footer.php
@@ -31,3 +31,12 @@
         </ul>
     </div>
 </div>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109783708-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-109783708-1');
+</script>
